33783cddefe81123b7598bf8c0bf4958a4e6d6b4
M-Arslan011 <arslanashraf185@gmail.com>
2024-08-11 13:37:37 +0500

    code

---
+++ src/components/category/Card.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Button from "../shared/Button";
+
+const Cards = ({
+  title,
+  subtile,
+  description,
+  buttonText,
+  buttonbgColor,
+  textColors,
+  cardbgColor,
+  imgSrc,
+  className = "",
+  imgclassName = "",
+  ...props
+}) => {
+  return (
+    <div
+      className={`py-10 pl-5 shadow-xl bg-gradient-to-br  from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end hover:transform hover:-translate-y-2 hover:shadow-lg transition-transform duration-300 ${cardbgColor} ${className}`}
+    >
+      <div>
+        <div className="mb-4">
+          <p className="mb-[2px] text-gray-400">{title}</p>
+          <p className="text-2xl font-semibold mb-[2px]">{subtile}</p>
+          <p className="text-4xl xl:text-5xl font-bold opacity-20 mb-2">
+            {description}
+          </p>
+          <Button
+            text={buttonText}
+            bgColor={buttonbgColor}
+            textColor={textColors}
+          />
+        </div>
+      </div>
+      <img
+        src={imgSrc}
+        className={`w-[320px] absolute bottom-0 ${imgclassName} `}
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Cards;
--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,8 +1,44 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-
+import Cards from "./Card";
+import img1 from "../../assets/category/earphone.png";
+import img2 from "../../assets/category/gaming.png";
+import img3 from "../../assets/category/watch.png";
 const Category = () => {
-  return <div>category</div>;
+  return (
+    <>
+      <div className="py-8">
+        <div className="container">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 ">
+            {/* 1st col */}
+            <Cards
+              title={"Enjoy"}
+              subtile={"with"}
+              description={"Earphone"}
+              buttonText={"Browse"}
+              buttonbgColor={"bg-primary"}
+              textColors={"text-white"}
+              imgSrc={img1}
+            />
+            {/* 2nd col */}
+            <Cards
+              title={"Enjoy"}
+              subtile={"with"}
+              description={"Gadget"}
+              buttonText={"Browse"}
+              buttonbgColor={"bg-primary"}
+              textColors={"text-white"}
+              imgSrc={img3}
+              cardbgColor={"bg-brandYellow"}
+              className="from-brandYellow/90 to-brandYellow/70"
+              imgclassName="-right-4 lg:top-[40px]"
+            />
+          </div>
+        </div>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Category;
